76826a963e9daba52de21ad3bdec18448c8a2b4b
Nicolò Santilio <nicolo.santilio@outlook.com>
2023-12-19 22:40:57 +0000

    shellcheck

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,11 @@ table=$(cat "$OUTPUT_PATH"/table)
 echo "passed=$passed" >> "$GITHUB_OUTPUT"
 
 EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
-echo "table<<$EOF" >> "$GITHUB_OUTPUT"
-echo "$table" >> "$GITHUB_OUTPUT"
-echo "$EOF" >> "$GITHUB_OUTPUT"
+{ 
+    echo "table<<$EOF"
+    echo "$table"
+    echo "$EOF" 
+}   >> "$GITHUB_OUTPUT"
 
 echo "$table" >> "$GITHUB_STEP_SUMMARY"
 
