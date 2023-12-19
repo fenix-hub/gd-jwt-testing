fcdb3d655dd80cd91f251ec438ef9965b4a77a53
Nicolò Santilio <nicolo.santilio@outlook.com>
2023-12-19 16:43:23 +0000

    fix multiline variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,12 @@ passed=$(cat $OUTPUT_PATH/passed)
 table=$(cat $OUTPUT_PATH/table)
 
 echo "passed=$passed" >> "$GITHUB_OUTPUT"
-echo "table=$table" >> "$GITHUB_OUTPUT"
+
+EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
+echo "table<<$EOF" >> $GITHUB_OUTPUT
+echo "$table" >> $GITHUB_OUTPUT
+echo "$EOF" >> $GITHUB_OUTPUT
+
 echo "$table" >> "$GITHUB_STEP_SUMMARY"
 
 exit 0
