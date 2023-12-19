e6a61439b60367d8752416b7319f8b4f757619ab
Nicolò Santilio <nicolo.santilio@outlook.com>
2023-12-19 15:44:06 +0000

    Fix linting

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ godot --headless --path src/ -s test.gd --disable-render-loop --quit
 
 passed=$(cat src/passed)
 table=$(cat src/table)
-echo "passed=$passed" >> $GITHUB_OUTPUT
-echo "table=$table" >> $GITHUB_OUTPUT
-echo "$table" >> $GITHUB_STEP_SUMMARY
+echo "passed=$passed" >> "$GITHUB_OUTPUT"
+echo "table=$table" >> "$GITHUB_OUTPUT"
+echo "$table" >> "$GITHUB_STEP_SUMMARY"
 
 exit 0
