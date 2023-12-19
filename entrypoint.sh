62aacc68353962305b4ac0af5cd500e184fce9f1
Nicolò Santilio <nicolo.santilio@outlook.com>
2023-12-19 22:26:22 +0000

    minor fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,15 @@
 
 godot --headless --path "/test/src" -s test.gd --disable-render-loop --quit
 
-passed=$(cat $OUTPUT_PATH/passed)
-table=$(cat $OUTPUT_PATH/table)
+passed=$(cat "$OUTPUT_PATH"/passed)
+table=$(cat "$OUTPUT_PATH"/table)
 
 echo "passed=$passed" >> "$GITHUB_OUTPUT"
 
 EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
-echo "table<<$EOF" >> $GITHUB_OUTPUT
-echo "$table" >> $GITHUB_OUTPUT
-echo "$EOF" >> $GITHUB_OUTPUT
+echo "table<<$EOF" >> "$GITHUB_OUTPUT"
+echo "$table" >> "$GITHUB_OUTPUT"
+echo "$EOF" >> "$GITHUB_OUTPUT"
 
 echo "$table" >> "$GITHUB_STEP_SUMMARY"
 
